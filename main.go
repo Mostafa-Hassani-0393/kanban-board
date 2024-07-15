dd24bd0d6394a851e7bcd3bdf662d05ab780c054
Mostafa <mostafahassani0393@gmail.com>
2024-07-16 02:06:09 +0330

    Small fix to Board constructor, implemented task constructor, simplified task model for now

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"KanbanBoard/Task"
 	"KanbanBoard/board"
 	"fmt"
 )
 
 func main() {
 	defaultBoard := board.NewBoard()
+	testTask := Task.NewTask("Test", "This is a test task")
 	fmt.Println(defaultBoard)
+	fmt.Println(testTask)
 
 }
